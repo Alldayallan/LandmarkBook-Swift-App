51c81bae335de57e04cca84821ea9c0904525f17
Allan <mail@allanreid.co>
2020-10-20 15:41:07 -0700

    delete function added

--- LandmarkBook/ViewController.swift
+++ LandmarkBook/ViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var landmarkNames = [String]()
+    var landmarkImages = [UIImage]()
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -17,31 +20,38 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.delegate = self
         tableView.dataSource = self
         
-        var landmarkNames = [String]()
+        //Landmark Book Data
+        
         landmarkNames.append("Colosseum")
         landmarkNames.append("Great Wall")
         landmarkNames.append("Kremlin")
         landmarkNames.append("Stonehenge")
         landmarkNames.append("Taj Mahal")
         
-        var landmarkImages = [UIImage]()
-        
         landmarkImages.append(UIImage(named: "colosseum.jpg")!)
         landmarkImages.append(UIImage(named: "greatwall.jpg")!)
         landmarkImages.append(UIImage(named: "kremlin.jpg")!)
         landmarkImages.append(UIImage(named: "stonehenge.jpg")!)
         landmarkImages.append(UIImage(named: "tajmahal.jpg")!)
-        
     }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            landmarkNames.remove(at: indexPath.row)
+            landmarkImages.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+        }
+    }
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "test"
+        cell.textLabel?.text = landmarkNames[indexPath.row]
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return landmarkNames.count
     }
 
 }
